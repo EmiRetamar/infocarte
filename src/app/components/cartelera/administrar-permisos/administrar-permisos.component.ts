7b6df3eff15eaa12752fe5540e93a58f97330b68
Emi Retamar <emiliano_retamar@hotmail.com.ar>
2019-03-18 03:56:59 -0300

    Terminando el otorgar permisos a profesores

--- src/app/components/cartelera/administrar-permisos/administrar-permisos.component.ts
+++ src/app/components/cartelera/administrar-permisos/administrar-permisos.component.ts
@@ -39,13 +39,8 @@ export class AdministrarPermisosComponent implements OnInit {
 								this.obtenerProfesores(users);
 								setTimeout(() => {
 									this.getCartelerasForProfesores(this.profesores);
-									/* Cuando la funcion anonima tiene una sola sentencia se puede expresar
-									sin llaves ni punto y coma */
-									/* "setTimeout()" recibe como parametro una funcion anonima y un valor
-									que representa el tiempo expresado en milisegundos. Este tiempo es el que
-									se espera para que se ejecuten las sentencias de la funcion anonima */
-									setTimeout(() => this.loaded = true, 1000);
-								}, 2000);
+									setTimeout(() => this.loaded = true, 1500);
+								}, 1000);
 							}
 						);
 				}
@@ -73,24 +68,14 @@ export class AdministrarPermisosComponent implements OnInit {
 	}
 
 	requestCartelerasForProfesor(profesor: Usuario) {
-		// Se inicializa el arreglo que va a guardar las carteleras que tiene permiso el profesor
-		/* Es un arreglo bidimensional donde el arreglo principal (la fila) representara a un profesor
-		y el arreglo secundario (la columna) representara una cartelera a la que tiene permiso el profesor */
 		this.cartelerasForProfesores[profesor.id] = new Array();
-		// Se obtienen los permisos del profesor
 		this.userService.getPermissions(profesor.id)
 			.subscribe(
 				(permissions) => {
 					for (let permission of permissions) {
-						/* Se obtienen las carteleras a las que tiene permisos el profesor, cada permiso
-						corresponde a una cartelera en la que tiene permisos */
 						this.userService.getBillboardForPermission(permission.id)
 							.subscribe(
 								(cartelera: Cartelera) => {
-									/* Cada profesor va a tener permisos en una o muchas carteleras
-									por lo tanto las carteleras a las que tiene permiso cada profesor
-									se guardan en una matriz donde los indices son el "id" del profesor
-									y el "id" de la cartelera a la que tiene permiso */
 									this.cartelerasForProfesores[profesor.id][cartelera.id] = cartelera;
 								}
 							);
@@ -100,9 +85,6 @@ export class AdministrarPermisosComponent implements OnInit {
 	}
 
 	hasPermissions(profesor: Usuario) {
-		/* Si el profesor actual tiene permisos en la cartelera actual se obtendra un valor
-		diferente de undefined (la cartelera actual) por lo tanto la expresion booleana retorna true
-		caso contrario, retorna false */
 		return (this.cartelerasForProfesores[profesor.id][this.cartelera.id] != undefined);
 	}
 
